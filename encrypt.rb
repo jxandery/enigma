112b6aa8de6a39b9b9c05d6b3b8be017ef5e0f85
Jack Yeh <jack.yeh@gmail.com>
2015-03-03 15:17:35 -0700

    offset methods working

--- encrypt.rb
+++ encrypt.rb
@@ -22,6 +22,29 @@
     key.to_s[3..4].to_i
   end
 
+  def squares_date(date)
+      (date.to_i) **2
+  end
+
+  def a_offset(date)
+    offsets = squares_date(date).to_s.chars
+    offsets[-4].to_i
+  end
+
+  def b_offset(date)
+    offsets = squares_date(date).to_s.chars
+    offsets[-3].to_i
+  end
+
+  def c_offset(date)
+    offsets = squares_date(date).to_s.chars
+    offsets[-2].to_i
+  end
+
+  def d_offset(date)
+    offsets = squares_date(date).to_s.chars
+    offsets[-1].to_i
+  end
 
 
  end
