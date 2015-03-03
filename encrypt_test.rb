08eb0f453cd04e8ce03fab93468f89a238bf10d6
Jack Yeh <jack.yeh@gmail.com>
2015-03-03 15:13:48 -0700

    test for a_offset method passing

--- encrypt_test.rb
+++ encrypt_test.rb
@@ -30,6 +30,35 @@ class EncryptTest < Minitest::Test
     assert_equal 21, encryptor.d_rotation
   end
 
+  def test_method_squares_date_with_first_char_as_zero
+    encryptor = Encrypt.new(41521)
+    assert_equal 412699225, encryptor.squares_date("020315")
+  end
+
+  def test_method_squares_date_with_first_char_not_zero
+    encryptor = Encrypt.new(41521)
+    assert_equal 14475699225, encryptor.squares_date("120315")
+  end
+
+  def test_a_offset_is_fourth_digit_from_end
+    encryptor = Encrypt.new(41521)
+    assert_equal 9, encryptor.a_offset("020315")
+  end
+
+  def test_b_offset_is_third_digit_from_end
+    encryptor = Encrypt.new(41521)
+    assert_equal 2, encryptor.b_offset("020315")
+  end
+
+  def test_c_offset_is_second_digit_from_end
+    encryptor = Encrypt.new(41521)
+    assert_equal 2, encryptor.c_offset("020315")
+  end
+
+  def test_d_offset_is_last_digit
+    encryptor = Encrypt.new(41521)
+    assert_equal 5, encryptor.d_offset("020315")
+  end
 
 end
 
