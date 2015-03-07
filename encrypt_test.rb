04790ae6d8265c91669b7df2c35ead45279678dc
Jack Yeh <jack.yeh@gmail.com>
2015-03-07 14:23:38 -0700

    rotation tests now pass

--- encrypt_test.rb
+++ encrypt_test.rb
@@ -31,19 +31,19 @@ class RotatorTest < Minitest::Test
   end
 
   def test_b_rotation_takes_second_two_digits_of_key
-    encryptor.stub :key, ("41521") do
+    rotator.stub :key, ("41521") do
       assert_equal 15, rotator.b_rotation
     end
   end
 
   def test_c_rotation_takes_third_two_digits_of_key
-    encryptor.stub :key, ("41521") do
+    rotator.stub :key, ("41521") do
       assert_equal 52, rotator.c_rotation
     end
   end
 
   def test_d_rotation_takes_fourth_two_digits_of_key
-    encryptor.stub :key, ("41521") do
+    rotator.stub :key, ("41521") do
       assert_equal 21, rotator.d_rotation
     end
   end
