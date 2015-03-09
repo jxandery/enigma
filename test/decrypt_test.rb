0b932ba863ee300808da0bdebdd62555cd21cfb2
Jack Yeh <jack.yeh@gmail.com>
2015-03-09 17:41:05 -0600

    after adding key as a parameter, a lot of the stubbing is no longer necessary.

--- test/decrypt_test.rb
+++ test/decrypt_test.rb
@@ -49,87 +49,55 @@ class DecryptTest < Minitest::Test
   end
 
   def test_combine_number_of_positions_moved_and_message_returns_correct_value_aka_message_decrypt_a_works
-    rotator.stub :a_rotation, (41) do
-      assert_equal "c", decryptor.message_decrypt_a("020315", "not ", "41521")
-    end
+    assert_equal "c", decryptor.message_decrypt_a("020315", "not ", "41521")
   end
 
   def test_message_decrypt_a_works_for_a_different_key
-   rotator.stub :a_rotation, (87) do
-     assert_equal "8", decryptor.message_decrypt_a("020315", "not ", "41521")
-   end
+   assert_equal "8", decryptor.message_decrypt_a("020315", "not ", "87521")
   end
 
   def test_message_decrypt_a_works_for_highest_possible_rotation_key_of_value_99
-    rotator.stub :a_rotation, (99) do
-      assert_equal "w", decryptor.message_decrypt_a("020315", "not ", "41521")
-    end
+    assert_equal "w", decryptor.message_decrypt_a("020315", "not ", "99521")
   end
 
   def test_message_decrypt_a_works_for_lowest_possible_rotation_key_of_value_00
-    rotator.stub :a_rotation, (00) do
-      assert_equal "e", decryptor.message_decrypt_a("020315", "not ", "41521")
-    end
+    assert_equal "e", decryptor.message_decrypt_a("020315", "not ", "00521")
   end
 
   def test_message_decrypt_a_works_for_a_different_date
-    rotator.stub :a_rotation, (41) do
-      assert_equal "l", decryptor.message_decrypt_a("220388", "not ", "41521")
-    end
+    assert_equal "l", decryptor.message_decrypt_a("220388", "not ", "41521")
   end
 
   def test_message_decrypt_a_works_for_lowest_possible_offset_value_0
-    rotator.stub :a_rotation, (41) do
-      assert_equal "l", decryptor.message_decrypt_a("010100", "not ", "41521")
-    end
+    assert_equal "l", decryptor.message_decrypt_a("010100", "not ", "41521")
   end
 
   def test_message_decrypt_a_works_for_highest_possible_offset_value_9
-    rotator.stub :a_rotation, (00) do
-      assert_equal "e", decryptor.message_decrypt_a("020315", "not ", "41521")
-    end
+    assert_equal "e", decryptor.message_decrypt_a("020315", "not ", "00521")
   end
 
   def test_message_decrypt_a_works_for_highest_possible_rotation_value_00_highest_possible_offset_value_0_and_first_unique_character_a
-    rotator.stub :a_rotation, (00) do
-      assert_equal "a", decryptor.message_decrypt_a("010100", "a not", "41521")
-    end
+    assert_equal "a", decryptor.message_decrypt_a("010100", "a not", "00521")
   end
 
   def test_message_decrypt_a_works_for_highest_possible_rotation_value_99_highest_possible_offset_value_9_and_last_unique_character_of_comma
-    rotator.stub :a_rotation, (99) do
-      assert_equal "i", decryptor.message_decrypt_a("020315", ",not", "41521")
-    end
+    assert_equal "i", decryptor.message_decrypt_a("020315", ",not", "99521")
   end
 
   def test_message_decrypt_b_works
-    rotator.stub :b_rotation, (15) do
-      assert_equal " ", decryptor.message_decrypt_b("020315", "not ", "41521")
-    end
+    assert_equal " ", decryptor.message_decrypt_b("020315", "not ", "41521")
   end
 
   def test_message_decrypt_c_works
-    rotator.stub :c_rotation, (52) do
-      assert_equal "e", decryptor.message_decrypt_c("020315", "not ", "41521")
-    end
+    assert_equal "e", decryptor.message_decrypt_c("020315", "not ", "41521")
   end
 
   def test_message_decrypt_d_works
-    rotator.stub :d_rotation, (21) do
-      assert_equal "k", decryptor.message_decrypt_d("020315", "not ", "41521")
-    end
+    assert_equal "k", decryptor.message_decrypt_d("020315", "not ", "41521")
   end
 
   def test_first_four_characters_as_a_group_decrypts
-    rotator.stub :a_rotation, (41) do
-      rotator.stub :b_rotation, (15) do
-        rotator.stub :c_rotation, (52) do
-          rotator.stub :d_rotation, (21) do
-            assert_equal ["c"," ","e","k"], decryptor.batch_decrypt("020315", "not ","41521")
-          end
-        end
-      end
-    end
+    assert_equal ["c"," ","e","k"], decryptor.batch_decrypt("020315", "not ","41521")
   end
 
   def test_message_splits_into_four_char_arrays
@@ -141,27 +109,11 @@ class DecryptTest < Minitest::Test
   end
 
   def test_it_returns_error_message_when_message_contains_invalid_character
-     rotator.stub :a_rotation, (41) do
-      rotator.stub :b_rotation, (15) do
-        rotator.stub :c_rotation, (52) do
-          rotator.stub :d_rotation, (21) do
-            assert_equal "invalid character in message", decryptor.decrypt("020315", "7vm*w2mapvsx45m6p mfz3txv564l4m1m7m04os0pxt4","41521")
-          end
-        end
-      end
-    end
+    assert_equal "invalid character in message", decryptor.decrypt("020315", "7vm*w2mapvsx45m6p mfz3txv564l4m1m7m04os0pxt4","41521")
   end
 
   def test_can_decrypt_two_batches
-    rotator.stub :a_rotation, (41) do
-      rotator.stub :b_rotation, (15) do
-        rotator.stub :c_rotation, (52) do
-          rotator.stub :d_rotation, (21) do
-            assert_equal "we all need to get some korean bbq at daegee", decryptor.decrypt("020315", "7vm0w2mapvsx45m6p mfz3txv564l4m1m7m04os0pxt4", "41521")
-          end
-        end
-      end
-    end
+    assert_equal "we all need to get some korean bbq at daegee", decryptor.decrypt("020315", "7vm0w2mapvsx45m6p mfz3txv564l4m1m7m04os0pxt4", "41521")
   end
 
 end
